3d5d37af4d161333d3d87b4772c4abf6a75af49b
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2020-04-13 00:13:16 -0300

    Handle time 1 minute ago

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,11 +1,17 @@
 import humanizedTime from '../lib/index.js';
+import { MINUTE } from '../lib/constants';
 
 it('Should handle time around now', () => {
-  const now = new Date();
+  const now = new Date(Date.now());
   expect(humanizedTime(now)).toBe('just now');
 });
 
 it('Should handle option to return an object', () => {
-  const now = new Date();
+  const now = new Date(Date.now());
   expect(humanizedTime(now, 'descriptive')).toEqual({ moment: 'now' });
+});
+
+it('Should handle a time 1 minute ago', () => {
+  const oneMinute = new Date(Date.now() - MINUTE);
+  expect(humanizedTime(oneMinute)).toBe('1 minute ago');
 });
